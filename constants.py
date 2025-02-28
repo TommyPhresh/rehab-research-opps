02f0d60dfed54adb72788526b02d3805a1ba6430
TommyPhresh <trich0315@gmail.com>
2025-02-27 20:21:07 -0500

    Clinical Trials constants from documentation

---
+++ constants.py
@@ -0,0 +1,8 @@
+# clinical trials constants 
+trials_url = "https://clinicaltrials.gov/api/v2/studies"
+trials_format = "csv"
+trials_statuses = ["NOT_YET_RECRUITING","RECRUITING",
+                   "AVAILABLE","ENROLLING_BY_INVITATION"]
+trials_pagesize = 1000
+# will nail down once universal format set
+# trials_columns = ["BriefTitle","Sponsor"]
