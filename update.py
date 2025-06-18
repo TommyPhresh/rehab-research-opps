50b196329e7cb72ecc50c15954693ddab40f602f
TommyPhresh <108419176+TommyPhresh@users.noreply.github.com>
2025-06-18 07:09:42 -0400

    created db update skeleton logic / pseudocode

---
+++ update.py
@@ -0,0 +1,39 @@
+from private_updaters import *
+from gov_updaters import *
+from functools import reduce
+
+"""
+Cache using parquet
+create is_fresh fn to check if its been enough time to update again
+create rebuild_cache fn to refresh cache
+create get_data fn to return a new list of dicts every time period
+
+call the get_data fn (which has cache check in it)
+this is super cheap 99% of the time
+
+use scheduler and add a job such that each period cache is refreshed
+
+call inside duckdb with original query 
+"CREATE TABLE_NAME VIEW table AS SELECT ..."
+"REFRESH TABLE_NAME VIEW table
+
+Pass to Jinja the same way I've been doing except Parquet not csv
+"""
+
+# FORMAT
+    # Name - award name
+    # Org - sponsoring organization
+    # Desc - brief description
+    # 
+# 
+def update():
+    data = get_data()
+
+def get_data():
+    data = []
+    privates = private_updaters_all
+    publics = gov_updaters_all
+    result = reduce(lambda f, func: func(f), privates, data)
+    return reduce(lambda f, func: func(f), publics, result)
+
+
